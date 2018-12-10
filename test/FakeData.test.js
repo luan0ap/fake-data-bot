277255757c021f00a9bca4d4a2b6562e50bae3a4
luan0ap <luanap.kun@gmail.com>
2018-12-10 15:54:22 -0200

    create test for email in randomize function

--- test/FakeData.test.js
+++ test/FakeData.test.js
@@ -1,23 +1,30 @@
 import FakeData from '../src'
 
-test('Randomized object', () => {
+describe('Tests from randomize function', () => {
   const fake = new FakeData({
-    name: '',
     email: '',
-    others: []
   })
 
-  const configRandomize = {
+  const configEmailTrue = {
     email: true,
-    array: 4
-  }
-  
-  const result = {
-    name: 'Pedrinho',
-    email: '12345@test.com'
-    other: [1,2,3,4]
   }
 
-  expect(fake.randomize(configRandomize)).toBe(result)
+  it('Randomize function returns a object', () => {
+    expect(typeof fake.randomize(configEmailTrue)).toBe('object')
+  })
+
+  it('Fake object and return by randomize function has same properties', () => {
+    expect(Object.getOwnPropertyNames(fake.randomize(configEmailTrue)))
+    .toEqual(Object.getOwnPropertyNames(fake.defaultData))
+  })
+
   
+  it('Return of function randomize with config in email equal true, is a valid email', () => {
+
+    const regexEmail = /^([^\W][\w\.\+-]*[^\W]*)@([^\W_]*[\w\.-]*[^\W_])$/
+
+    expect(fake.randomize(configEmailTrue)['email'])
+      .toEqual(expect.stringMatching(regexEmail))
+  })
 })
+
