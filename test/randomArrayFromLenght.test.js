7926f6c9e314a33bf75870fd21cfa157a680a9e9
luan0ap <luanap.kun@gmail.com>
2018-12-12 14:46:43 -0200

    Create test for random array form length

--- test/randomArrayFromLenght.test.js
+++ test/randomArrayFromLenght.test.js
@@ -1,1 +1,9 @@
 import randomArrayFromLenght from '../src/utils/randomArrayFromLenght'
+
+test('Must return a random array with lenght by parameter', () => {
+  const LENGTH = 5
+  const randomArray = randomArrayFromLenght(LENGTH)
+  
+  expect(Array.isArray(randomArray).toBe(true)
+  expect(randomArray.length).toEqual(LENGTH)
+}
